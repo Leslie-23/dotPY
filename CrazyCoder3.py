1a255de5274909e01eb1187a2a9af915255ae63b
leslie-23 <leslieajayi27@gmail.com>
2024-07-25 19:20:04 +0000

    new adds-2

---
+++ CrazyCoder3.py
@@ -0,0 +1,1 @@
+# will put code here in the next commit
--- web-scrape.py
+++ web-scrape.py
@@ -19,8 +19,8 @@ def extract_data(soup):
     data = []
     articles = soup.find_all('article')
     for article in articles:
-        title = article.find('h1').text
-        link = article.find('a').get('href')
+        title = article.find('h1').text()
+        link = article.find('h3').text()
         data.append({'title': title, 'link': link})
     return data
 
@@ -29,7 +29,7 @@ def save_to_csv(data, filename):
     df.to_csv(filename, index=False)
 
 def main():
-    url = 'https://www.wikipedia.org/'
+    url = 'https://paltech-weather02.netlify.app/'
     html_content = fetch_web_page(url)
     if html_content:
         soup = parse_html(html_content)
